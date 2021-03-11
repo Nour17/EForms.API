dbc8eed168b59013b689b66d9f422c3bc58b2666
Noureldin <nour.tarek177@gmail.com>
2021-03-11 15:32:47 +0200

    Layers structure is done and working

--- WebApplication2/Startup.cs
+++ WebApplication2/Startup.cs
@@ -8,7 +8,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace EForms.API.Infrastructure
+namespace WebApplication2
 {
     public class Startup
     {
